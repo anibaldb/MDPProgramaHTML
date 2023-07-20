ebe479e19bea8185d0f5d623d039e6a874427a54
anibaldb <128552893+anibaldb@users.noreply.github.com>
2023-07-19 23:46:24 -0300

    java

---
+++ Trabajo1-Clase1Js/JavaScript.js
@@ -0,0 +1,14 @@
+let nom = 'Anibal'
+console.log(nom)
+
+let a= 5
+let b= 6
+console.log(a+b)
+
+let edad = 40
+
+console.log('Su nombre es '+nom+' y su edad es de '+edad+' años')
+
+
+let comp = (a<edad) ? a : edad
+console.log('el menor de los dos valores es '+comp)
